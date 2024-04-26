8e34d7fdb6b840f791794d700ce19b9eb4d694f7
hahahumble <hexichn@gmail.com>
2024-04-26 15:40:23 +0800

    feat: semantic html for probability page

--- app/(home)/probability/page.tsx
+++ app/(home)/probability/page.tsx
@@ -5,15 +5,24 @@ import Calculator from '@/components/calculator';
 function Page() {
   return (
     <>
-      <View paddingBottom={12} paddingTop={24} width={{ s: '100%', m: 130 }}>
-        <Text weight="medium" variant="featured-3">
-          概率计算器
-        </Text>
-        <Text variant="body-2" color="neutral-faded">
-          根据真实的出生人口数据，计算投胎的可能性
-        </Text>
+      <View as="main">
+        <View
+          paddingBottom={12}
+          paddingTop={24}
+          width={{ s: '100%', m: 130 }}
+          as="header"
+        >
+          <Text weight="medium" variant="featured-3" as="h1">
+            概率计算器
+          </Text>
+          <Text variant="body-2" color="neutral-faded" as="h2">
+            根据真实的出生人口数据，计算投胎的可能性
+          </Text>
+        </View>
+        <View as="article">
+          <Calculator />
+        </View>
       </View>
-      <Calculator />
     </>
   );
 }
